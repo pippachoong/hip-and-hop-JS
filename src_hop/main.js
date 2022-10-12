dde3b8e0a08e02ba0e96c484d27f6aa396d3455a
Justin Tan <justintws10@gmail.com>
2022-10-12 11:07:45 +1100

    login js worked

--- src_hop/main.js
+++ src_hop/main.js
@@ -7,7 +7,7 @@ export default new Phaser.Game({
   // Phaser.AUTO meaning Phaser will decide to use Canvas or WebGL mode depending on the browser and devide.
   
   type: Phaser.AUTO,
-  width: 1080,
+  width: 480,
   height: 640,
   scene: [Game, GameOver],
   physics: {
@@ -16,7 +16,7 @@ export default new Phaser.Game({
       gravity: {
         y: 200
       },
-      // debug: true //enables collision boxes
+      debug: true //enables collision boxes
     }
   }
 })
---
+++ src_login/login.js
@@ -0,0 +1,51 @@
+// import { axios } from "../src_lib/axios"
+
+console.log('loaded')
+console.log('loaded2')
+
+// let BASE_URL;
+// if( process.env.NODE_ENV === 'development'){
+//   BASE_URL = 'http://localhost:3000';
+// } else {
+//   BASE_URL = 'https://hipandhop.herokuapp.com';
+// }
+
+let BASE_URL = 'http://localhost:3000'
+
+$( function(){
+
+  $('#login-form').on('submit', function(ev){
+
+    ev.preventDefault()
+    const email = $('#email').val()
+    const password = $('#password').val()
+
+    handleSubmit(email, password)
+
+  }) 
+
+})
+
+const handleSubmit = async (email, password) => {
+
+  console.log('handleSubmit:',email, password)
+
+  try {
+
+    const res = await axios.post(`${BASE_URL}/login`, {
+      email: email,
+      passwordDigest: password
+    })
+
+    console.log(`post response:`, res.data)
+
+    window.location.replace('/home.html')
+
+  }catch(err){
+    console.log(`error logging in:`, err)
+    const $errorMsg = $('<p>Incorrect login credentials. Please try again.</p>')
+    $('body').append($errorMsg)
+  }
+
+
+}
