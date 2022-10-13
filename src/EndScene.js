c8c39178075f49444a5330e42deee9ca9af42873
Philippa Choong <pippachoong@gmail.com>
2022-10-13 11:11:46 +1100

    show highest score on leaderboard

--- src/EndScene.js
+++ src/EndScene.js
@@ -23,45 +23,49 @@ export default class EndScene extends Phaser.Scene {
         })
             .then(res => {
                 console.log('new score:', res.data)
+                // On successfully posting high score, fetch latest score
+                axios.get(`${BASE_URL}/hip-scores`)
+                    .then(res => {
+
+                        console.log(res.data)
+                        const results = res.data
+                        let textContent = ["GAME____OVER", "", "name      score", ""]
+
+                        for (let i = 0; i < results.length; i++) {
+                            const perLineResults = `${results[i].name}_____${results[i].hipScore}`
+
+                            textContent.push(perLineResults)
+
+                        }
+                        console.log('textcontent', textContent)
+
+                        const width = this.scale.width
+                        const height = this.scale.height
+                        console.log('width and height', width, height);
+                        this.add.text(width * 0.5, height * 0.5, textContent, {
+                            fontSize: 30
+                        })
+                            .setOrigin(0.5)
+
+                    })
+                    .catch(err => {
+                        console.log('error getting scores:', err)
+                    })
             })
             .catch(err => {
                 console.log('Error posting results:', err)
             })
 
         // this is the axios get to grab the top 5 user name and their score, and show it in a screen
-        axios.get(`${BASE_URL}/hip-scores`, {
-        })
-            .then(res => {
-
-                console.log(res.data)
-                const results = res.data
-                let textContent = ["GAME____OVER", "", "name      score", ""]
-
-                for (let i = 0; i < results.length; i++) {
-                    const perLineResults = `${results[i].name}_____${results[i].hipScore}`
-
-                    textContent.push(perLineResults)
-
-                }
-                console.log('textcontent', textContent)
-
-                const width = this.scale.width
-                const height = this.scale.height
-                console.log('width and height', width, height);
-                this.add.text(width * 0.5, height * 0.5, textContent, {
-                    fontSize: 30
-                })
-                    .setOrigin(0.5)
-
-            })
-            .catch(err => {
-                console.log('error getting scores:', err)
-            })
 
 
         this.input.keyboard.once('keydown-SPACE', () => {
             this.scene.start('game')
         })
+
+        // add restart button
+        this.restart = this.add.image(0, 80, 'restart').setInteractive();// setInteractive() add event handlers
+
     }
 }
 
